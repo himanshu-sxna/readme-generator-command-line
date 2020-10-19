c27cbaf8da19a222bbb18a34fd4073ebf2721f0f
Himanshu Saxena <contact@hellohimanshu.com>
2020-10-19 23:07:07 +1030

    fixed geberated Readme file format

---
+++ demoReadme.md
@@ -0,0 +1,38 @@
+# Demo Project
+
+![License Badge](https://img.shields.io/badge/license-MIT-green)
+
+
+## Table of Contents 
+
+- [Descriptiion(#description)
+-[License](#license)
+-[Installation](#installation)
+-[Usage](#usage)
+-[Tests](#tests)
+-[Developer Info](#devInfo)
+
+## <a name="description"></a>Description
+
+This is a demo description
+
+
+## <a name="license"></a>License
+MIT
+## <a name="installation"></a>Installation
+
+No instrcutions required
+
+
+## <a name="usage"></a>Usage
+
+Not Applicable
+
+
+## <a name="tests"></a>Tests
+
+Not Required
+
+
+## <a name="devInfo> Developer Info
+
--- modules/generateReadme.js
+++ modules/generateReadme.js
@@ -4,68 +4,45 @@ module.exports = {
     generateReadme: (inquirerResponse, axiosResponse) => {
 
 
-        let tableOfContents = `## Table of Contents
-            
-                                            - [Descriptiion(#description)
-                                            -[License](#license)`;
+        let tableOfContents = `## Table of Contents \n\n- [Descriptiion(#description)\n-[License](#license)\n`;
 
         if (inquirerResponse.install !== ""){
-            tableOfContents += `-[Installation](#installation)`
+            tableOfContents += `-[Installation](#installation)\n`
         };
 
         if (inquirerResponse.usage !== ""){
-            tableOfContents += `-[Usage](#usage)`
+            tableOfContents += `-[Usage](#usage)\n`
         };
 
         if (inquirerResponse.tets !== ""){
-            tableOfContents += `-[Tests](#tests)`
+            tableOfContents += `-[Tests](#tests)\n`
         }
 
-        tableOfContents += `-[Developer Info](#devInfo)`;
+        tableOfContents += `-[Developer Info](#devInfo)\n\n`;
 
         let readmeFile = 
-            `# ${inquirerResponse.title}
-
-            ![License Badge](https://img.shields.io/badge/license-${inquirerResponse.license}-green)`
+            `# ${inquirerResponse.title}\n\n![License Badge](https://img.shields.io/badge/license-${inquirerResponse.license}-green)\n\n\n`
 
             readmeFile += tableOfContents;
 
-            `## <a name="description"></a>Description
-
-            ${inquirerResponse.desc}
-            
-            ## <a name="license"></a>License
-            
-            ${inquirerResponse.license}`
+            readmeFile+= `## <a name="description"></a>Description\n\n${inquirerResponse.desc}\n\n\n## <a name="license"></a>License\n${inquirerResponse.license}\n`;
 
             if (inquirerResponse.install !== '') {
   
-                readmeFile +=`
-                ## <a name="installation"></a>Installation
-                
-                *Steps required to install project and how to get the development environment running:*
-                
-                ${inquirerResponse.install}`
+                readmeFile +=`## <a name="installation"></a>Installation\n\n${inquirerResponse.install}\n\n\n`
                 };
 
                 if (inquirerResponse.usage !== '') {
   
-                    readmeFile +=`
-                    ## <a name="usage"></a>Usage
-            
-                    
-                    ${inquirerResponse.usage}`
+                    readmeFile +=`## <a name="usage"></a>Usage\n\n${inquirerResponse.usage}\n\n\n`
                     };
 
                     if (inquirerResponse.tests !== '') {
   
-                        readmeFile +=`
-                        ## <a name="tests"></a>Tests
-                        
-                        ${inquirerResponse.tests}`
+                        readmeFile +=`## <a name="tests"></a>Tests\n\n${inquirerResponse.tests}\n\n\n`
                         };
         
-                        readmeFile += `## <a name="devInfo> Developer Info`;
+                        readmeFile += `## <a name="devInfo> Developer Info\n\n`;
 
         return readmeFile;
     }
